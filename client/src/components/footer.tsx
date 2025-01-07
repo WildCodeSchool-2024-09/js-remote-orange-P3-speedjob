24511b1c60578ef3f367b3417e5c7c8bb1bc6f19
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2025-01-07 17:22:39 +0100

    Footer content + tailwind setup

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer>
+      <ul className="flex flex-row content-around text-3xl font-bold underline">
+        <li>Retour en haut de la page</li>
+        <li>Contacts</li>
+        <li>Mentions Légales</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
