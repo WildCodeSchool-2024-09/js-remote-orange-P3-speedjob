85e36f77f5fd2dc28eee08f85e7ea138e9e3d304
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2025-01-09 15:51:33 +0100

    adding login module

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,11 +1,9 @@
-import SignUpModule from "../components/SignUpModule";
 import SignInModule from "../components/signInModule";
 
 function Login() {
   return (
     <div>
       <h1>Login Page</h1>
-      {/* <SignUpModule /> */}
       <SignInModule />
     </div>
   );
