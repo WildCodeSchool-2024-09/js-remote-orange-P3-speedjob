571ab9ca965b7980f7c24163e6d98dad91a0fc1e
giovannidavid <giovannidavid67@gmail.com>
2025-01-09 10:09:42 +0100

    ajout searchbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Footer from "./components/footer";
+import Searchbar from "./components/searchbar"
 
 function App() {
   return (
     <div className="App">
+      <Searchbar />
       <Footer />
     </div>
   );
