642836da82d72721dde69ecc6861d4ef1179e53c
Badre-s <bsahili@hotmail.fr>
2025-01-10 11:14:33 +0100

    About Home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Legal from "./components/legal";
+import AboutHome from "./components/aboutHome";
 
 function App() {
-  return (
-    <div className="App">
-      <Legal />
-    </div>
-  );
+	return (
+		<div className="App">
+			<AboutHome />
+			<Legal />
+		</div>
+	);
 }
 
 export default App;
