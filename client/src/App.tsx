6f5018ff870fe4ebdf6081a5ae5681e8547a475a
Badre-s <bsahili@hotmail.fr>
2025-01-10 10:07:56 +0100

    Add page about

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 import Legal from "./components/legal";
 
 function App() {
-  return (
-    <div className="App">
-      <Legal />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Legal />
+		</div>
+	);
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,22 +14,26 @@ import App from "./App";
 // import About from "./pages/About";
 // import Contact from "./pages/Contact";
 import Legal from "./pages/legalPage";
-
+import About from "./pages/aboutPage";
 
 /* ************************************************************************* */
 
 // Create router configuration with routes
 // You can add more routes as you build out your app!
 const router = createBrowserRouter([
-  {
-    path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
-  },
-  {
-    path: "/legalPage",
-    element: <Legal />,
-  },
-  // Try adding a new route! For example, "/about" with an About component
+	{
+		path: "/", // The root path
+		element: <App />, // Renders the App component for the home page
+	},
+	{
+		path: "/legalPage",
+		element: <Legal />,
+	},
+	{
+		path: "/AboutPage",
+		element: <About />,
+	},
+	// Try adding a new route! For example, "/about" with an About component
 ]);
 
 /* ************************************************************************* */
@@ -37,14 +41,14 @@ const router = createBrowserRouter([
 // Find the root element in the HTML document
 const rootElement = document.getElementById("root");
 if (rootElement == null) {
-  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
 
 // Render the app inside the root element
 createRoot(rootElement).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 );
 
 /**
