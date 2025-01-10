d8a54b195f6fb79a25fdf8bfc2199d0b1428ef13
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2025-01-10 11:59:44 +0100

    adding random job module & jobboard page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./components/header";
 import Blog from "./pages/blog";
 import Contact from "./pages/contact";
 import Home from "./pages/home";
+import Jobboard from "./pages/jobboard";
 import Legal from "./pages/legal";
 import Login from "./pages/login";
 import NotFound from "./pages/notFound";
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/jobboard" element={<Jobboard />} />
           <Route path="/legal" element={<Legal />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signIn" element={<SignIn />} />
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,10 +1,10 @@
 import RandomArticle from "../components/randomArticle";
+import RandomJob from "../components/randomJob";
 
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1>Home</h1>
-      <h2>Random Job</h2>
+      <RandomJob />
       <RandomArticle />
       <h2>About Us</h2>
     </div>
