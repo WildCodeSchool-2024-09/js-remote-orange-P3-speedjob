1fca7aef7c2f3e0df9d69c39987e46da019b587f
Badre-s <bsahili@hotmail.fr>
2025-01-30 18:06:48 +0100

    conflicts resolved

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 const router = express.Router();
 
 import adminActions from "./modules/admin/adminActions";
@@ -11,16 +12,40 @@ import userActions from "./modules/user/userActions";
 
 router.get("/", (req, res) => console.log("Hello World"));
 
+// Configuration de Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (
+  req: express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only images are allowed."));
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
 // Définition des routes
 router.get("/api/user", userActions.browse);
-router.get("/api/user/:id", userActions.read);
+router.get("/api/user/:id([0-9]+)", userActions.read);
 router.post("/api/user", userActions.add);
-router.put("/api/user/:id", userActions.edit);
-router.delete("/api/user/:id", userActions.destroy);
+router.put("/api/user/:id([0-9]+)", userActions.edit);
+router.delete("/api/user/:id([0-9]+)", userActions.destroy);
 
 router.get("/api/company", companyActions.browse);
 router.get("/api/company/:id", companyActions.read);
-router.post("/api/company", companyActions.add);
+router.post("/api/company", upload.single("logo"), companyActions.add);
 router.put("/api/company/:id", companyActions.edit);
 router.delete("/api/company/:id", companyActions.destroy);
 
@@ -37,10 +62,11 @@ router.put("/api/admin/:id", adminActions.edit);
 router.delete("/api/admin/:id", adminActions.destroy);
 
 router.get("/api/annonces", annoncesActions.browse);
-router.get("/api/annonces/:id", annoncesActions.read);
+router.get("/api/annonces/:id([0-9]+)", annoncesActions.read);
+router.get("/api/annonces/search", annoncesActions.search);
 router.post("/api/annonces", annoncesActions.add);
-router.put("/api/annonces/:id", annoncesActions.edit);
-router.delete("/api/annonces/:id", annoncesActions.destroy);
+router.put("/api/annonces/:id([0-9]+)", annoncesActions.edit);
+router.delete("/api/annonces/:id([0-9]+)", annoncesActions.destroy);
 
 router.get("/api/articles", articlesActions.browse);
 router.get("/api/articles/:id", articlesActions.read);
@@ -53,6 +79,7 @@ import SignIn from "./modules/auth/auth";
 router.post("/api/auth/signin", SignIn.SignIn);
 router.post("/api/auth/signup", SignIn.SignUp);
 router.get("/api/auth/check", SignIn.Check);
+
 /* ************************************************************************* */
 
 export default router;
